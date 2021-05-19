132d9d338d3c6e4eefeb6cbbe7b8ca5cfe9dfa16
Irvin Buendia <irvin.buendia1@gmail.com>
2021-05-19 10:17:07 -0500

    added loading screen

--- js/movie-application.js
+++ js/movie-application.js
@@ -1,7 +1,47 @@
-const getMovies = () => {
+
+const loader = document.querySelector("#loading");
+
+
+
+
+// showing loading
+function displayLoading() {
+    $(loader).addClass("display");
+
+    // to stop loading after some time
+    setTimeout(() => {
+        $(loader).removeClass("display");
+    }, 5000);
+}
+
+// hiding loading
+function hideLoading() {
+    $(loader).removeClass("display");
+}
+
+
+// // showing loading
+// function displayLoading() {
+//     loader.classList.add("display");
+//     // to stop loading after some time
+//     setTimeout(() => {
+//         loader.classList.remove("display");
+//     }, 5000);
+// }
+//
+// // hiding loading
+// function hideLoading() {
+//     loader.classList.remove("display");
+// }
+
+
+function fetchHandler(event) {
+    displayLoading()
+
     fetch('https://abundant-automatic-knee.glitch.me/movies')
         .then(response => response.json())
         .then(movies => {
+            hideLoading()
             console.log(movies)
             let htmlStr = "";
             for(let movie of movies){
@@ -10,6 +50,29 @@ const getMovies = () => {
             $("#container").html(htmlStr)
 
         });
-};
+}
 
-getMovies()
+$(document).ready( function () {
+    fetchHandler(event);
+});
+
+
+//
+// const getMovies = () => {
+//     fetch('https://abundant-automatic-knee.glitch.me/movies')
+//         .then(response => response.json())
+//         .then(movies => {
+//
+//             console.log(movies)
+//             let htmlStr = "";
+//             for(let movie of movies){
+//                 htmlStr += `<h1>${movie.title}</h1><p> Directed by: ${movie.director} <br> <img src="${movie.poster}"> </p>`
+//             }
+//             $("#container").html(htmlStr)
+//
+//         });
+// };
+//
+// getMovies()
+
+
